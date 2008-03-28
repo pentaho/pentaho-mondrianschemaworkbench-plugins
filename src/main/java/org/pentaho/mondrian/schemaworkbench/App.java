09603b3837ed94f60fa26cca9788e5f28a1109cf
Mat Lowery <mlowery@pentaho.com>
2008-03-28 17:29:11 +0000

    Initial commit.

---
+++ src/main/java/org/pentaho/mondrian/schemaworkbench/App.java
@@ -0,0 +1,13 @@
+package org.pentaho.mondrian.schemaworkbench;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
