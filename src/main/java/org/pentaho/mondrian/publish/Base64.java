7efa557f0c2aaa2cc1852e88a3a63f41a3f44c2e
Pentaho Build Guy <buildgy@pentaho.com>
2013-09-18 13:22:05 +0000

    reverted licenese header mods

--- src/main/java/org/pentaho/mondrian/publish/Base64.java
+++ src/main/java/org/pentaho/mondrian/publish/Base64.java
@@ -1,4 +1,5 @@
 /*
+// $Id: //open/mondrian/testsrc/main/mondrian/util/Base64Test.java#2 $
 // This software is subject to the terms of the Common Public License
 // Agreement, available at the following URL:
 // http://www.opensource.org/licenses/cpl.html.
@@ -6,7 +7,6 @@
 // All Rights Reserved.
 // You must accept the terms of that agreement to use this software.
 */
-
 package org.pentaho.mondrian.publish;
 
 /**
