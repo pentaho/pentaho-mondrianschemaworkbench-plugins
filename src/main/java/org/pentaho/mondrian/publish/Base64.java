f4b8d01dc90f3d50edfe36b23a5229549d8e1065
Pentaho Build Guy <buildgy@pentaho.com>
2013-09-19 15:52:00 +0000

    [PPP-3081] license header updates on all .java, .js, and .jsp

--- src/main/java/org/pentaho/mondrian/publish/Base64.java
+++ src/main/java/org/pentaho/mondrian/publish/Base64.java
@@ -1,5 +1,4 @@
 /*
-// $Id: //open/mondrian/testsrc/main/mondrian/util/Base64Test.java#2 $
 // This software is subject to the terms of the Common Public License
 // Agreement, available at the following URL:
 // http://www.opensource.org/licenses/cpl.html.
@@ -7,6 +6,7 @@
 // All Rights Reserved.
 // You must accept the terms of that agreement to use this software.
 */
+
 package org.pentaho.mondrian.publish;
 
 /**
