81cfd13d79b1829df297463e525cdc33831843c7
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-11-11 13:38:42 -0500

    DEVO-11194: updated copyright headers

--- src/main/java/org/pentaho/mondrian/publish/workbench/PublishUtil.java
+++ src/main/java/org/pentaho/mondrian/publish/workbench/PublishUtil.java
@@ -1,3 +1,14 @@
+/*! ******************************************************************************
+ *
+ * Pentaho
+ *
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
+ *
+ * Change Date: 2029-07-20
+ ******************************************************************************/
 package org.pentaho.mondrian.publish.workbench;
 
 import com.sun.jersey.api.client.Client;
